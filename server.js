9dc472b2f06267d50740d1e0df482db98f161e41
William Phokompe <phokompe.william@outlook.com>
2019-09-09 21:47:46 +0200

    Modify DBConnection string to lessen the number of env variables

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  process.env.URI.toString() + '&' + process.env.MAJORITY.toString(),
+  `${process.env.URI.toString()}&w=majority`,
   {
     useNewUrlParser: true,
     useFindAndModify: false
